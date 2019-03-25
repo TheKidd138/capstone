4c54a1a6923afdc55f60c03f48c2cde80b3e36b2
TheKidd138 <garrettkidd237@gmail.com>
2019-03-25 19:20:09 -0400

    oops, needed to save current file

--- app/webapp/refurbco/models.py
+++ app/webapp/refurbco/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Repair(models.Model):
+
+
+class Client(models.Model):
+
+
+class Part(models.Model):
+
+
+class Invoice(models.Model):
+
+
+class Order(models.Model):
+
+
+class Quote(models.Model):
+
+
