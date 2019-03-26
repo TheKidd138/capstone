ed3912eea2e451e4f69866d844b65eb930143120
TheKidd138 <garrettkidd237@gmail.com>
2019-03-26 09:54:14 -0400

    added DB views to the admin page

--- app/webapp/refurbco/admin.py
+++ app/webapp/refurbco/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import RepairTicket, Order, Inventory, Repair, Invoice
 
 # Register your models here.
+
+admin.site.register(RepairTicket)
+admin.site.register(Order)
+admin.site.register(Inventory)
+admin.site.register(Repair)
+admin.site.register(Invoice)
